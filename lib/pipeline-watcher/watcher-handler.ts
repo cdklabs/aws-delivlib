ce744c05ab77831f70fc031ed3cb12a314701e6b
Sam Goodwin <38672686+sam-goodwin@users.noreply.github.com>
2018-12-13 02:00:55 -0800

    Raise an alarm when any stages are in a Failed state. (#6)
    Configure a lambda function to watch the pipeline and emit a metric for the number of failed stages, enabling consistent alarming on a 'failed pipeline'.

---
+++ lib/pipeline-watcher/watcher-handler.ts
@@ -0,0 +1,34 @@
+import AWS = require('aws-sdk');
+
+// export for tests
+export const codePipeline = new AWS.CodePipeline();
+export const logger = {
+  log: (line: string) => process.stdout.write(line)
+};
+
+/**
+ * Lambda function for checking the stages of a CodePipeline and emitting log
+ * entries with { failedCount = <no. of failed stages> } for async metric
+ * aggregation via metric filters.
+ *
+ * It requires the pipeline's name be set as the 'PIPELINE_NAME' environment variable.
+ */
+export async function handler() {
+  const pipelineName = process.env.PIPELINE_NAME;
+  if (!pipelineName) {
+    throw new Error("Pipeline name expects environment variable: 'PIPELINE_NAME'");
+  }
+  const state = await codePipeline.getPipelineState({
+    name: pipelineName
+  }).promise();
+
+  let failedCount = 0;
+  if (state.stageStates) {
+    failedCount = state.stageStates
+      .filter(stage => stage.latestExecution !== undefined && stage.latestExecution.status === 'Failed')
+      .length;
+  }
+  logger.log(JSON.stringify({
+    failedCount
+  }));
+}
