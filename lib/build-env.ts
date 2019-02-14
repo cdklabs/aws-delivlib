f3368601a9d072a09e339c88e9121ffaaebf33fa
Elad Ben-Israel <benisrae@amazon.com>
2019-02-14 13:12:39 +0200

    use Superchain

--- lib/build-env.ts
+++ lib/build-env.ts
@@ -1,6 +1,6 @@
 import cbuild = require('@aws-cdk/aws-codebuild');
 import cdk = require('@aws-cdk/cdk');
-import path = require('path');
+import { Superchain } from './superchain';
 
 export interface BuildEnvironmentProps {
   computeType?: cbuild.ComputeType;
@@ -9,14 +9,12 @@ export interface BuildEnvironmentProps {
   buildImage?: cbuild.IBuildImage;
 }
 
-export function createBuildEnvironment(parent: cdk.Construct, props: BuildEnvironmentProps) {
+export function createBuildEnvironment(scope: cdk.Construct, props: BuildEnvironmentProps) {
   const environment: cbuild.BuildEnvironment = {
     computeType: props.computeType || cbuild.ComputeType.Small,
     privileged: props.privileged,
     environmentVariables: renderEnvironmentVariables(props.env),
-    buildImage: props.buildImage || cbuild.LinuxBuildImage.fromAsset(parent, 'superchain', {
-      directory: path.join(__dirname, '..', 'superchain')
-    })
+    buildImage: props.buildImage || new Superchain(scope).buildImage
   };
 
   return environment;
