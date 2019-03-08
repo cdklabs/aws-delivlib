a791ecdef751ce7fd5b1d253447bf92efcbd049d
Rico Huijbers <huijbers@amazon.com>
2019-03-08 16:46:43 +0100

    feat(s3): make S3 publisher idempotent
    S3 publisher can be made idempotent by giving it a unique token for
    every version in a config file. Already-published revisions will not be
    published again.
    
    Switch to 's3 cp' instead of 's3 sync' to avoid reading the server-side
    files; no need to do any syncing, we'll either do a full copy of all
    files or no files.

--- lib/publishing/s3/publish.sh
+++ lib/publishing/s3/publish.sh
@@ -10,4 +10,37 @@ args=""
 if ${PUBLIC:-false}; then
   args="--acl public-read"
 fi
-aws s3 sync . $BUCKET_URL $args
+
+idempotency_token=""
+
+# See if there's a file with publishing commands
+if [[ -f s3-publishing.json ]]; then
+    echo "Found publishing instructions"
+    cat s3-publishing.json
+
+    idempotency_token=$(node -pe "require('./s3-publishing.json')['idempotency-token'] || ''")
+
+    # We don't want to upload this file
+    args="$args --exclude s3-publishing.json"
+fi
+
+if [[ "${idempotency_token:-}" != "" ]]; then
+    echo "Idempotency token: $idempotency_token"
+
+    aws s3 ls $BUCKET_URL/$idempotency_token > /dev/null && {
+        echo "Token found, stopping."
+        exit 0
+    } || {
+        echo "Idempotency token not found, continuing."
+    }
+fi
+
+# Do the copy
+echo "Starting the upload to $BUCKET_URL"
+echo "(Args: $args)"
+aws s3 cp --recursive . $BUCKET_URL $args
+
+if [[ "${idempotency_token:-}" != "" ]]; then
+    echo "Writing idempotency token..."
+    echo 1 | aws s3 cp - $BUCKET_URL/$idempotency_token
+fi
