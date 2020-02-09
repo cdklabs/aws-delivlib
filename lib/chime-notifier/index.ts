8b95c625108e1897c667c6c8e511c1f296df104f
Rico Huijbers <rix0rrr@gmail.com>
2020-02-09 08:15:32 +0100

    feat: add Chime notifications on failure (#237)
    feat: add Chime notifications on failure
    
    Add a list of Chime webhooks which will be notified when there is a
    build failure in the pipeline.
    
    
    -----
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

---
+++ lib/chime-notifier/index.ts
@@ -0,0 +1,1 @@
+export * from './chime-notifier';
---
+++ test/chime-notifier.test.ts
@@ -0,0 +1,59 @@
+import * as https from 'https';
+jest.mock('https');
+
+const mockHttpsWrite = jest.fn();
+(https as any).request = jest.fn((_url, _options, cb) => {
+  return {
+    on: jest.fn(),
+    write: mockHttpsWrite,
+    end: () => cb({
+      statusCode: 200,
+      headers: {},
+      setEncoding: () => undefined,
+      on: (event: string, listener: () => void) => {
+        if (event === 'end') { listener(); }
+      }
+    })
+  };
+});
+
+import { codePipeline, handler } from '../lib/chime-notifier/notifier-handler';
+
+test('call codepipeline and then post to webhooks', async () => {
+  process.env.WEBHOOK_URLS = 'https://my.url/';
+
+  codePipeline.getPipelineExecution = jest.fn().mockReturnValue({
+    promise: () => Promise.resolve({
+      "pipelineExecution": {
+        "pipelineExecutionId": "xyz",
+        "pipelineVersion": 1,
+        "pipelineName": "xyz",
+        "status": "Succeeded",
+        "artifactRevisions": [
+          {
+            "revisionUrl": "revision.com/url",
+            "revisionId": "1234",
+            "name": "Source",
+            "revisionSummary": "A thing happened"
+          }
+        ]
+      }
+    })
+  });
+
+  await handler({
+    "detail": {
+        "pipeline": "myPipeline",
+        "version": "1",
+        "state": "FAILED",
+        "execution-id": "abcdef"
+    }
+  });
+
+  expect(https.request).toBeCalledWith('https://my.url/', expect.objectContaining({
+    method: 'POST'
+  }), expect.any(Function));
+  expect(mockHttpsWrite).toBeCalledWith(expect.stringContaining('"Content"')); // Contains JSON
+  expect(mockHttpsWrite).toBeCalledWith(expect.stringContaining('myPipeline')); // Contains the pipeline name
+  expect(mockHttpsWrite).toBeCalledWith(expect.stringContaining('A thing happened')); // Contains the revision summary
+});
