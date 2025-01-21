0d2c80654148b0f18dcca4cf5f227cc6ade9f8ed
Momo Kornher <kornherm@amazon.co.uk>
2025-01-21 18:34:22 +0000

    feat(package-integrity): can pass env vars to integrity build
    Some builds will require env vars set to run a build correctly or authenticate with upstream services.
    Allow passing through all types of CodeBuild env vars to the underlying Shellable job.

--- lib/__tests__/package-integrity/integrity.test.ts
+++ lib/__tests__/package-integrity/integrity.test.ts
@@ -7,7 +7,6 @@ import { Template, Match } from 'aws-cdk-lib/assertions';
 import { LinuxPlatform, PackageIntegrityValidation } from '../..';
 
 test('creates a codebuild project that triggers daily and runs the integrity handler', () => {
-
   const stack = new Stack(new App(), 'TestStack');
   const token = sm.Secret.fromSecretCompleteArn(stack, 'GitHubSecret', 'arn:aws:secretsmanager:us-east-1:123456789123:secret:github-token-000000');
 
@@ -61,25 +60,39 @@ test('creates a codebuild project that triggers daily and runs the integrity han
       ]),
     },
   });
+});
 
-  // expect(template.Resources.IntegrityD83C2C0B.Properties.Environment.EnvironmentVariables.slice(2)).toStrictEqual(
-  //   [
-  //     {
-  //       Name: 'GITHUB_REPOSITORY',
-  //       Type: 'PLAINTEXT',
-  //       Value: 'cdklabs/some-repo',
-  //     },
-  //     {
-  //       Name: 'TAG_PREFIX',
-  //       Type: 'PLAINTEXT',
-  //       Value: '',
-  //     },
-  //     {
-  //       Name: 'GITHUB_TOKEN_ARN',
-  //       Type: 'PLAINTEXT',
-  //       Value: 'arn:aws:secretsmanager:us-east-1:123456789123:secret:github-token-000000',
-  //     },
-  //   ],
-  // );
+test('can pass environment variables to the integrity handler code build project', () => {
+  const stack = new Stack(new App(), 'TestStack');
+  const token = sm.Secret.fromSecretCompleteArn(stack, 'GitHubSecret', 'arn:aws:secretsmanager:us-east-1:123456789123:secret:github-token-000000');
 
+  new PackageIntegrityValidation(stack, 'Integrity', {
+    buildPlatform: new LinuxPlatform(codebuild.LinuxBuildImage.fromDockerRegistry('jsii/superchain:1-bullseye-slim-node14')),
+    githubTokenSecret: token,
+    repository: 'cdklabs/some-repo',
+    environment: {
+      FOO: 'bar'
+    },
+    environmentSecrets: {
+      SECRET: 'arn:aws:secretsmanager:us-east-1:123456789123:secret:super-secret-token-000000'
+    }
+  });
+
+  const template = Template.fromStack(stack);
+  template.hasResourceProperties('AWS::CodeBuild::Project', {
+    Environment: {
+      EnvironmentVariables: Match.arrayWith([
+        {
+          Name: 'FOO',
+          Type: 'PLAINTEXT',
+          Value: 'bar',
+        },
+        {
+          Name: 'SECRET',
+          Type: 'SECRETS_MANAGER',
+          Value: 'super-secret-token',
+        },
+      ]),
+    },
+  });
 });
